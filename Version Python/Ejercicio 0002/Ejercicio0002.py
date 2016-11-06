777a9e1eb6e57ff37f33ccf39fb682d15da4799a
silverfox78 <sbarrera@gmail.com>
2016-11-06 00:58:23 -0300

    Ejercicio 2

---
+++ Version Python/Ejercicio 0002/Ejercicio0002.py
@@ -0,0 +1,33 @@
+# Clase de ejercicio 0001
+
+import json
+from pprint import pprint
+class Ejercicio0002:
+    """Esta es la clase del ejercicio 2"""
+
+    def __init__(self, archivo):
+        with open(archivo) as data_file:    
+            data = json.load(data_file)
+        self.inicio = data['inicio']
+        self.actual = data['actual']
+        self.limite = data['limite']
+
+    def getSuma(self, salida):
+        suma = 0
+        total = self.inicio + self.actual
+        while self.actual < self.limite:
+            suma = self.inicio + self.actual
+            self.inicio = self.actual
+            self.actual = suma
+            #print("inicio: " + str(self.inicio) + " - actual: " + str(self.actual) + " - suma: " + str(suma))
+            if suma % 2 == 0:
+                total += suma;
+        data = {"Total": total}
+        with open(salida, 'w') as outfile:
+            json.dump(data, outfile)
+        return data
+
+retorno = Ejercicio0002('./data.json').getSuma('./resultado.json')
+
+for key, value in retorno.items():
+    print (key + ": " + str(format(value, ',d')))
--- Version Python/readme.md
+++ Version Python/readme.md
@@ -1,7 +1,7 @@
 #Listado de ejecicios :memo:
 
 - [x] 1	Multiples of 3 and 5
-- [ ] 2	Even Fibonacci numbers	
+- [x] 2	Even Fibonacci numbers	
 - [ ] 3	Largest prime factor	
 - [ ] 4	Largest palindrome product	
 - [ ] 5	Smallest multiple	
