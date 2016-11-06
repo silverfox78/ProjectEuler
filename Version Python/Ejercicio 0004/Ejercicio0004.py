8def5b27eba1e945fea031180ffa05a23566052a
silverfox78 <sbarrera@gmail.com>
2016-11-06 16:47:08 -0300

    Actualizacion de la informacion

--- Version Python/Ejercicio 0004/Ejercicio0004.py
+++ Version Python/Ejercicio 0004/Ejercicio0004.py
@@ -9,9 +9,8 @@ class Ejercicio0004:
         with open(archivo) as data_file:    
             data = json.load(data_file)
         self.inicio = data['inicio']
-        self.fin = data['fin']
+        self.fin = data['fin'] - 1
         self.factores = []
-        self.palindromo = 0
 
     def esPalindromo(self, numero):
         lista = list(str(numero))
@@ -19,18 +18,51 @@ class Ejercicio0004:
             return True
         return False
 
+    def getLista(self):
+        listapares = []
+        for multiplicando in range(self.fin, self.inicio, -1):
+            for multiplicador in range(multiplicando, self.inicio, -1):
+                listapares.append((multiplicando, multiplicador, multiplicando * multiplicador))
+        return list(listapares)
+
     def determinaPalindromo(self, salida):
-        for multiplicando in range(self.inicio, self.fin):
-            for multiplicador in range(multiplicando, self.fin):
-                numero = multiplicando * multiplicador
-                if self.esPalindromo(numero) and (numero > self.palindromo):
-                    self.palindromo = numero
-                    data = {"Palindromo": self.palindromo, "Multiplicando": multiplicando, "Multiplicador": multiplicador}
-        with open(salida, 'w') as outfile:
-            json.dump(data, outfile)
-        return data
+        from operator import itemgetter
+        listado = sorted(self.getLista(), key=lambda x: x[2], reverse=True)
+        for key, value, producto in listado:
+            if self.esPalindromo(producto):
+                data = {"Palindromo": producto, "Multiplicando": key, "Multiplicador": value}
+                with open(salida, 'w') as outfile:
+                    json.dump(data, outfile)
+                return data
+
+# ranges = [(n, min(n+1, 10), n *min(n+1, 10)) for n in range(1, 10, 1)]
+# print(ranges)
+
+# it=iter(range(1000,100,-1))
+# it1=iter(range(1000,100,-1))
+# lst = [(next(it),next(it1)) for _ in range(10)]
+# from operator import itemgetter
+# listado = sorted(lst,key=lambda x: x[1], reverse=True)
+# for key, value in listado:
+#     #for key, value, producto in sorted(Ejercicio0004('./data.json').getLista().items()):
+#     #print (key + ": " + str(format(value, ',d')) + " - Producto: " + str(format(producto, ',d')))
+#     print (str(key) + ": " + str(value))
+
+
+# lista = Ejercicio0004('./data.json').getLista()
+
+# from operator import itemgetter
+# listado = sorted(lista,key=lambda x: x[2], reverse=True)
+
+# for key, value, producto in listado:
+# #for key, value, producto in sorted(Ejercicio0004('./data.json').getLista().items()):
+#     #print (key + ": " + str(format(value, ',d')) + " - Producto: " + str(format(producto, ',d')))
+#     print (str(key) + ": " + str(value) + " - Producto: " + str(producto))
+# #print(lista)
 
 retorno = Ejercicio0004('./data.json').determinaPalindromo('./resultado.json')
+
+
 
 for key, value in sorted(retorno.items()):
     print (key + ": " + str(format(value, ',d')))
