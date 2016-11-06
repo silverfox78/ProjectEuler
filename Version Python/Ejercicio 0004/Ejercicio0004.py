3d45c9af02f8fdcc71436adf8f276d12ce79e11e
silverfox78 <sbarrera@gmail.com>
2016-11-06 16:47:47 -0300

    Actualizacion de la informacion

--- Version Python/Ejercicio 0004/Ejercicio0004.py
+++ Version Python/Ejercicio 0004/Ejercicio0004.py
@@ -10,7 +10,6 @@ class Ejercicio0004:
             data = json.load(data_file)
         self.inicio = data['inicio']
         self.fin = data['fin'] - 1
-        self.factores = []
 
     def esPalindromo(self, numero):
         lista = list(str(numero))
@@ -35,34 +34,7 @@ class Ejercicio0004:
                     json.dump(data, outfile)
                 return data
 
-# ranges = [(n, min(n+1, 10), n *min(n+1, 10)) for n in range(1, 10, 1)]
-# print(ranges)
-
-# it=iter(range(1000,100,-1))
-# it1=iter(range(1000,100,-1))
-# lst = [(next(it),next(it1)) for _ in range(10)]
-# from operator import itemgetter
-# listado = sorted(lst,key=lambda x: x[1], reverse=True)
-# for key, value in listado:
-#     #for key, value, producto in sorted(Ejercicio0004('./data.json').getLista().items()):
-#     #print (key + ": " + str(format(value, ',d')) + " - Producto: " + str(format(producto, ',d')))
-#     print (str(key) + ": " + str(value))
-
-
-# lista = Ejercicio0004('./data.json').getLista()
-
-# from operator import itemgetter
-# listado = sorted(lista,key=lambda x: x[2], reverse=True)
-
-# for key, value, producto in listado:
-# #for key, value, producto in sorted(Ejercicio0004('./data.json').getLista().items()):
-#     #print (key + ": " + str(format(value, ',d')) + " - Producto: " + str(format(producto, ',d')))
-#     print (str(key) + ": " + str(value) + " - Producto: " + str(producto))
-# #print(lista)
-
 retorno = Ejercicio0004('./data.json').determinaPalindromo('./resultado.json')
-
-
 
 for key, value in sorted(retorno.items()):
     print (key + ": " + str(format(value, ',d')))
